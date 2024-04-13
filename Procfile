c8c923a55ecfbd04206c0b012f6753fd3c466eec
OdeliaCohen <odeliacohen1503@gmail.com>
2024-04-13 04:47:59 +0200

    test

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: heroku-php-apache2
+release: composer install --no-interaction --optimize-autoloader
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\HttpFoundation\Request;
+
+// Utilisation de "require_once" pour éviter l'inclusion multiple du fichier
+require_once dirname(__DIR__).'/vendor/autoload.php';
+
+// Chargement des variables d'environnement (une seule fois suffit)
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+
+// Activation du mode debug
+if ($_SERVER['APP_DEBUG']) {
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+}
+
+// Initialisation du kernel de l'application Symfony
+$kernel = new App\Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+
+// Gestionnaire de requête
+$request = Request::createFromGlobals();
+
